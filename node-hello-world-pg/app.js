76224c438851f67de9da1e6b4376511b040b42be
Tommy Hughes <tohughes@redhat.com>
2022-02-08 16:52:30 -0600

    node pg app
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- node-hello-world-pg/app.js
+++ node-hello-world-pg/app.js
@@ -1,6 +1,6 @@
 var { Client } = require('pg')
 var serviceBindings = require('kube-service-bindings');
-var ConnectionOptions = {}
+
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -9,14 +9,24 @@ var indexRouter = require('./routes/index');
 var greetingRouter = require('./routes/greeting');
 var app = express();
 
+var ConnectionOptions = {
+    username: 'user',
+    host: 'host',
+    database: 'database',
+    password: 'password',
+    port: 'port',
+}
 try {
     // check if the deployment has been bound to a pg instance through
     // service bindings. If so use that connect info
-    ConnectionOptions = serviceBindings.getBinding('POSTGRESQL', 'pg');
+    pgOptions = serviceBindings.getBinding('POSTGRESQL', 'pg');
+    ConnectionOptions.username = pgOptions.username
+    ConnectionOptions.host = pgOptions.host
+    ConnectionOptions.database = pgOptions.database
+    ConnectionOptions.password = pgOptions.password
+    ConnectionOptions.port = pgOptions.port
 } catch (err) { // proper error handling here
 };
-const client = new Client(ConnectionOptions)
-
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -29,6 +39,7 @@ app.use('/greeting', greetingRouter);
 
 module.exports = app;
 
+const client = new Client(ConnectionOptions)
 start()
 
 async function start() {
