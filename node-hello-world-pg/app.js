c87a3c3777bfa2d012a85b85fb967608f826baae
Tommy Hughes <tohughes@redhat.com>
2022-02-08 15:47:10 -0600

    node pg app
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- node-hello-world-pg/app.js
+++ node-hello-world-pg/app.js
@@ -1,36 +1,39 @@
-var { Client } = require('pg')
-var serviceBindings = require('kube-service-bindings');
+import { Client } from 'pg';
+var client = new Client()
+import { getBinding } from 'kube-service-bindings';
 
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var indexRouter = require('./routes/index');
-var greetingRouter = require('./routes/greeting');
+import express, { json, urlencoded, static } from 'express';
+import { join } from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import indexRouter from './routes/index';
+import greetingRouter from './routes/greeting';
 var app = express();
 
 try {
     // check if the deployment has been bound to a pg instance through
     // service bindings. If so use that connect info
-    pgConnectionBindings = serviceBindings.getBinding('POSTGRESQL', 'pg');
+    pgConnectionBindings = getBinding('POSTGRESQL', 'pg');
 } catch (err) { // proper error handling here
 };
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(json());
+app.use(urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(static(join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/greeting', greetingRouter);
 
-module.exports = app;
+export default app;
 
-var client = new Client()
-await client.connect()
+start()
 
-var res = await client.query('SELECT $1::text as message', ['Hello world!'])
-console.log(res.rows[0].message) // Hello world!
-console.log(client.host)
-await client.end()
+async function start() {
+    await client.connect()
+    var res = await client.query('SELECT $1::text as message', ['Hello world!'])
+    console.log(res.rows[0].message) // Hello world!
+    console.log(client.host)
+    await client.end()
+}
