9cf69065a34980e1e742844e717817976edd7d2a
Tommy Hughes <tohughes@redhat.com>
2022-02-08 15:13:14 -0600

    node pg app
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- node-hello-world-pg/app.js
+++ node-hello-world-pg/app.js
@@ -10,6 +10,16 @@ var indexRouter = require('./routes/index');
 var greetingRouter = require('./routes/greeting');
 
 var app = express();
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/greeting', greetingRouter);
+
+module.exports = app;
 
 try {
     // check if the deployment has been bound to a pg instance through
@@ -25,14 +35,3 @@ const res = await client.query('SELECT $1::text as message', ['Hello world!'])
 console.log(res.rows[0].message) // Hello world!
 console.log(client.host)
 await client.end()
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/greeting', greetingRouter);
-
-module.exports = app;
